a179f37e6389ca7bb9a06c6ec2f3459a083faf30
thabisile <thabisilemkhize@gmail.com>
2024-11-04 14:31:39 +0200

    Router.jsx file

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -11,14 +11,73 @@ const router = createBrowserRouter([
             },
             {
                 path: "/orders",
-                element: <div>Orders</div>
+                element: <PrivateRoute><OrderPage/></PrivateRoute>
             },
             {
                 path: "/about",
                 element: <div>About</div>
+
+            },
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/register",
+                element: <Register/>
+            },
+            {
+                path: "/cart",
+                element: <CartPage/>
+            },
+            {
+                path: "/checkout",
+                element: <PrivateRoute><CheckoutPage/></PrivateRoute>
+            },
+            {
+                path: "/books/:id",
+                element: <SingleBook/>
+            },
+            {
+                path: "/user-dashboard",
+                element: <PrivateRoute><UserDashboard/></PrivateRoute>
             }
         ]
     },
+    {
+        path: "/admin",
+        element: <AdminLogin/>
+    },
+    {
+        path: "/dashboard",
+        element: <AdminRoute>
+            <DashboardLayout/>
+        </AdminRoute>,
+        children:[
+            {
+                path: "",
+                element: <AdminRoute><Dashboard/></AdminRoute>
+            },
+            {
+                path: "add-new-book",
+                element: <AdminRoute>
+                    <AddBook/>
+                </AdminRoute>
+            },
+            {
+                path: "manage-books",
+                element: <AdminRoute>
+                    <ManageBooks/>
+                </AdminRoute>
+            },
+            {
+                path: "edit-book/:id",
+                element: <AdminRoute>
+                    <UpdateBook/>
+                </AdminRoute>
+            }
+        ]
+    }
 ]);
 
 export default router;
