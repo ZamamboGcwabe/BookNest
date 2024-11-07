b16d272608d6f2f263f1e5cf69e5f64e1f2078c8
thabisile <thabisilemkhize@gmail.com>
2024-11-07 13:10:12 +0200

    Updates

---
+++ backend/src/stats/admin.stats.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const Order = require('../orders/order.model');
+const Book = require('../books/book.model');
+const router = express.Router();
