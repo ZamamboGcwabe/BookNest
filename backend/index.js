9fc9221ab5e03da8e19096d19f853123220ddbb6
thabisile <thabisilemkhize@gmail.com>
2024-11-08 23:09:02 +0200

    Updates

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-
+const cors = require("cors");
 
 const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:"]
+}))
+
+const bookRoutes = require('./src/books/book.route')
+app.use("/api/books", bookRoutes)
 
 async function main() {
     await mongoose.connect(process.env.DB_URL);
