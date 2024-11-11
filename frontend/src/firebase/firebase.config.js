edd7fb4c869082d60542b383fab0f0f2c1f9ce32
thabisile <thabisilemkhize@gmail.com>
2024-11-11 14:07:26 +0200

    Firebase file

---
+++ frontend/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD_b5sYM8sVkbF8qZ8W7MIlQ26rjHMgOsQ",
+    authDomain: "booknest-mern-app.firebaseapp.com",
+    projectId: "booknest-mern-app",
+    storageBucket: "booknest-mern-app.firebasestorage.app",
+    messagingSenderId: "28543134687",
+    appId: "1:28543134687:web:ebcf7cc27ba6513416f26a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
