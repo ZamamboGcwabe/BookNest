fbc6cdd6b6cbea9843655735613dcdc2facf0107
thabisile <thabisilemkhize@gmail.com>
2024-11-11 19:08:04 +0200

    updates

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const User = require('./user.model');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-router.post("/admin",async (req, res) => {
-    const {username, password} = req.body;
+const JWT_SECRET = process.env.JWT_SECRET_KEY
+
+router.post("/admin", async (req, res) => {
+    const { username, password } = req.body;
     try {
-        const admin = await User.findOne({username});
-        if(!admin) {
-            res.status(404).send({message: "Invalid password"})
+        const admin = await User.findOne({ username });
+        if (!admin) {
+            res.status(404).send({ message: "Admin not found!" })
         }
+        if (admin.password !== password) {
+            res.status(401).send({ message: "Invalid password!" })
+        }
+
+        const token = jwt.sign(
+            { id: admin._id, username: admin.username, role: admin.role },
+            JWT_SECRET,
+            { expiresIn: "1h" }
+        )
 
         return res.status(200).json({
             message: "Authentication successful",
@@ -19,9 +31,10 @@ router.post("/admin",async (req, res) => {
                 role: admin.role
             }
         })
+
     } catch (error) {
         console.error("Failed to login as admin", error)
-        res.status(401).send({message: "Failed to login as admin"})
+        res.status(401).send({ message: "Failed to login as admin" })
     }
 })
 
